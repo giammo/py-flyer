a47b1adcd5b5157a72933b143966869699a15e80
giammo <giammo92@hotmail.it>
2023-10-15 23:42:05 +0200

    sistemato venv

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\Users\giammo\Downloads\Works\Programmazione\developer_workspace\genikem_volantini\venv
+set VIRTUAL_ENV=D:\Users\giammo\Downloads\Works\Programmazione\developer_workspace\py-flyer\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
